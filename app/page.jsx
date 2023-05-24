51aadb91d52adbc3f616c9c8eeb3222bb1c8ac3f
RonaldoDairo <programaciondairo@gmail.com>
2023-05-25 00:18:29 +0200

    obtain info from api reqres.inm

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+async function fetchUsers() {
+  const res = await fetch("https://reqres.in/api/users");
+  const data = await res.json();
+  return data.data
+}
 
-function IndexPage() {
-  return (
+
+async function IndexPage() {
+ const users = await fetchUsers();
+ 
+ return (
     <div>
       <h1>IndexPage</h1>
+      <div>
+        {JSON.stringify(users)}
+      </div>
     </div>
   )
 }

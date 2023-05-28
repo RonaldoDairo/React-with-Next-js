db76d059d45dbc5e93d149a12f03cc83edcd310b
RonaldoDairo <programaciondairo@gmail.com>
2023-05-28 13:24:41 +0200

    progress

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+async function getUser(id) {
+    const res = await fetch(`https://reqres.in/api/users/${id}`)
+    const data = await res.json()
+    return data.data
+}
 
-const UserPage = () => {
+ 
+const UserPage = async( {params} ) => {
+   
+    const user = await getUser(params.id)
+    console.log(user,"data")
+    
     return (
         <div>
-            <h1>User Details</h1>
+            <img src={user.avatar} alt={user.email} ></img>
+
+            <div>
+                <h3>{user.id} {user.first_name} {user.last_name}</h3>
+                <p>{user.email}</p>
+           </div>
+
         </div>
     );
 }
